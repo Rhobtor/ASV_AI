ffa718e2ad3a6c265d0d8cd4d54a7b243a09ef2d
rhobtor <luismidp7@gmail.com>
2024-03-19 11:59:43 +0100

    clean the code

--- asv_ai_workspace/src/camera_controller/camera_controller/camera_node.py
+++ asv_ai_workspace/src/camera_controller/camera_controller/camera_node.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image, CameraInfo
+import message_filters
 import numpy as np
 import quaternion
 import cv2
@@ -19,10 +20,9 @@ import torch
 from torch.hub import load
 from geometry_msgs.msg import Point
 from std_msgs.msg import String
-# import tf2_ros
-# from tf2_ros import TransformListener
-# from tf2_geometry_msgs import do_transform_pose
-# from tf2_py import Quaternion
+from ultralytics import YOLO
+from scipy.spatial.transform import Rotation as R
+
 
 class CameraTransformNode(Node):
 
@@ -61,57 +61,46 @@ class CameraTransformNode(Node):
         timer_period=1.0
         self.object_coordinates_timer=self.create_timer(timer_period,self.real_coordinates)
         self.object_distances_timer=self.create_timer(timer_period,self.object_dect)
-        self.model = load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        self.model = YOLO('./utils/yolov5su.engine', task='detect')
         self.bridge = CvBridge()
-        self.distance=0
-        self.u=0
-        self.v=0
+        np.bool = np.bool_
+        self.model(np.zeros((640,640,3)))
+        self.rotMat = r = R.from_rotvec(np.pi/2*np.array([1,0,1]))
+        self.distance=None
+        self.i = 0  
         self.cv_image=None
         self.depth_image=None
-        self.roll=None
-        self.pitch=None
-        self.yaw=None
-        self.RAD2DEG = 57.295779513
+        self.point_cloud_data=None
 
-    def pose_callback(self, msg):
-        # Camera position in map frame
-        tx = msg.pose.position.x
-        ty = msg.pose.position.y
-        tz = msg.pose.position.z
 
-        # Orientation quaternion
-        q = quaternion.quaternion(
-            msg.pose.orientation.w,
-            msg.pose.orientation.x,
-            msg.pose.orientation.y,
-            msg.pose.orientation.z
-        )
 
-        # Convert quaternion to roll, pitch, and yaw
-        self.roll, self.pitch, self.yaw = quaternion.as_euler_angles(q)
+    def image_callback(self, camera_info_msg, camera_image_msg, depth_msg):
 
-    def camera_info_callback(self, msg):
-        # Almacenar la información de la cámara
-        self.camera_info = msg
+        self.camera_info = camera_info_msg # To retrieve calibration data
+        self.cv_image = self.bridge.imgmsg_to_cv2(camera_image_msg, desired_encoding='bgr8') # left camera color image to pass YOLO
 
-    # def camera_image_callback(self,msg):
-    #     self.images=memoryview(msg.data).cast('f')
-
-    def image_callback(self, msg):
-        self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-        # if self.cv_image is None:
-        #     self.get_logger().info("No image available yet")
-            
-    
-        # results = self.model(self.cv_image)
-        # self.draw_boxes(self.cv_image, results.xyxy[0])
-        # cv2.imshow("Object Detection", self.cv_image)
-        # cv2.waitKey(1)
-    
-    def depth_callback(self, msg):
         # Get a pointer to the depth values casting the data pointer to floating point
-        self.depth_image = memoryview(msg.data).cast('f')
-        self.width=msg.width
+        self.depth_image = memoryview(depth_msg.data).cast('f')
+        self.width=depth_msg.width
+        
+        # Infer with YOLO
+        np.bool = np.bool_ # Quick fix for some deprecation problems
+        self.get_logger().info(f"------------------------------------------------------------------------------\n")
+        results = self.model(self.cv_image)
+
+        detected_classes = [results[0].names[i] for i in results[0].boxes.cls.to('cpu').numpy()]
+        bounding_boxes = results[0].boxes.xyxy.to('cpu').numpy()
+        self.distance = self.calculate_distance(detected_classes, bounding_boxes)
+
+        #self.draw_boxes(self.cv_image, results.xyxy[0])
+        cv2.imshow("Object Detection", results[0].plot(conf=False))
+        for cls in self.distance.keys():
+            self.get_logger().info(f"Class: {cls}, Distance: {self.distance[cls]} meters")
+        cv2.waitKey(1)
+
+        #self.publisher_.publish(msg)
+        #self.get_logger().info('Publishing: "%s"' % msg.data)
+
     def object_dect(self):
         if self.cv_image is None:
             self.get_logger().info("No image available yet")
@@ -151,108 +140,40 @@ class CameraTransformNode(Node):
             distance_msg.data = f"Class: {cls_conf:.2f}, Distance: {self.distance} meters"
             # self.publisher.publish(distance_msg)
 
-    def calculate_distance(self, xmin, ymin, xmax, ymax):
+    def calculate_distance(self, detected_classes, bboxes): # returns a dict with the distance of each detection in meters
         if self.depth_image is None:
-            return 0
-
-        center_x = int((xmin + xmax) / 2)
-        center_y = int((ymin + ymax) / 2)
-        depth_image_np = np.array(self.depth_image)
-        depth_values = depth_image_np[center_y * self.width + center_x]
-        depth_values = depth_values[depth_values != 0]
-
-        if len(depth_values) > 0:
-            return np.mean(depth_values)
-        else:
-            return 0
+            return -1
         
-    def real_coordinates(self):
+        real_distance = {cls_id: np.nan for cls_id in detected_classes}
+        for i, cls in enumerate(detected_classes):
+            xmin, ymin, xmax, ymax= bboxes[i] 
+            center_x = (xmin + xmax) // 2
+            center_y = (ymin + ymax) // 2
+            depth_image_np = np.array(self.depth_image)
+            depth_value = depth_image_np[int(center_y * self.width + center_x)]
 
-        # Convert pixel coordinates to 3D point in camera frame
-        Z = self.distance
-        X = (self.u - 663.865) * Z / (533.615)
-        Y = (self.v - 364.0325) * Z / (533.66)
+            real_dist, _ = self.real_coordinates(depth_value,center_x,center_y)
+            real_distance[cls] = real_dist
+        
+        return real_distance
+        
+    def real_coordinates(self, z_distance, u, v):
+
+        # Convert pixel coordinates to 3D point in camera frame (Right handed, z-up, x-forward (ROS))
+        Z = z_distance
+        X = (u - self.camera_info.k[2]) * Z / (self.camera_info.k[0]) # from https://github.com/stereolabs/zed-ros2-wrapper/blob/master/zed_components/src/zed_camera/src/zed_camera_component.cpp line 3067
+        Y = (v - self.camera_info.k[5]) * Z / (self.camera_info.k[4])
         point_camera_frame = np.array([X, Y, Z])
 
-        #self.get_logger().info("Point Frame (Base Frame): %s " %  str(point_camera_frame))
+        point_camera_shifted = self.rotMat.apply(point_camera_frame)
 
-        # Definir los ángulos de rotación en radianes
-        angle_z = np.radians(90)  # Rotación alrededor del eje z
-        angle_y = np.radians(90)  # Rotación alrededor del nuevo eje y
-
-        # Matrices de rotación
-        rotation_matrix_z = np.array([
-            [np.cos(angle_z), -np.sin(angle_z), 0],
-            [np.sin(angle_z), np.cos(angle_z), 0],
-            [0, 0, 1]
-        ])
-
-        rotation_matrix_y = np.array([
-            [np.cos(angle_y), 0, np.sin(angle_y)],
-            [0, 1, 0],
-            [-np.sin(angle_y), 0, np.cos(angle_y)]
-        ])
-
-        # Combinar las matrices de rotación
-        combined_rotation_matrix = np.dot(rotation_matrix_y, rotation_matrix_z)
-
-        # Aplicar las rotaciones a las coordenadas en el marco de la cámara
-        point_camera_rotated = np.dot(combined_rotation_matrix, point_camera_frame)
-
-        # Desplazamiento en el eje y y z
-        displacement_y = 0.3922
-        displacement_z = 0.2401
-
-        # Aplicar desplazamientos
-        point_camera_shifted = point_camera_rotated + np.array([0, displacement_y, displacement_z])
-
-        self.get_logger().info("Point Center Camera: %s " %  str(point_camera_shifted))
-
+        #self.get_logger().info("Point Center Camera: %s " %  str(point_camera_shifted))
+        real_distance = np.sqrt(X**2 + Y**2 + Z**2)
         
-###### por ahora no############################
-        # # Convertir de latitud y longitud a coordenadas UTM, (sub a gps node ASV)
-        # latitude = 37.4191666
-        # longitude = -6.0005347
-        # utm_coords = utm.from_latlon(latitude, longitude)
-        # utm_x, utm_y, zone_number, zone_letter = utm_coords
+        return real_distance, point_camera_shifted
 
-        # #print(f'UTM Coordinates: ({utm_x}, {utm_y}) in zone {zone_number}{zone_letter}')
 
-        # # Convertir de coordenadas UTM a latitud y longitud
-        # back_to_latlon = utm.to_latlon(point_camera_shifted[0]+utm_x, point_camera_shifted[1]+utm_y, zone_number, zone_letter)
-        # back_latitude, back_longitude = back_to_latlon
 
-        # #print(f'Back to Latitud, Longitude: ({back_latitude}, {back_longitude})')
-
-        # ##
-########################################################        
-        
-        rotation_matrix = np.array([
-            [np.cos(self.yaw), -np.sin(self.yaw), 0],
-            [np.sin(self.yaw), np.cos(self.yaw), 0],
-            [0, 0, 1]
-                ])
-
-        # Aplicar la rotación a la posición del objeto
-        rotated_obj_position = np.dot(rotation_matrix,point_camera_shifted )
-        self.get_logger().info("ASV poitn: %s " %  str(rotated_obj_position))
-
-       # Capture the image with the point marked
-        # self.visualize_point(msg, u, v)
-
-    def visualize_point(self, msg, u, v):
-        # Convert ROS Image message to OpenCV format
-        img = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
-
-        # Mark the point with a circle
-        img_with_point = img.copy()
-        cv2.circle(img_with_point, (u, v), 30, (0, 0, 0), -1)  # Circle in red
-
-         # Save the image in the script's folder
-        image_path = os.path.join(self.script_folder, 'marked_image.png')
-        cv2.imwrite(image_path, img_with_point)
-
-        self.get_logger().info(f"Image saved at: {image_path}")
 
 
 
